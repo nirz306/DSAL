6b17bdda6952ff735f62050aa28df5adbd5e23c3
Neeraj Magdum <122902068+nirz306@users.noreply.github.com>
2024-05-05 09:26:26 +0530

    Create 22_Heap_student marks.cpp

---
+++ 22_Heap_student marks.cpp
@@ -0,0 +1,72 @@
+ #include <iostream>
+using namespace std;
+
+class Student{
+    private: 
+        int marks;
+        string name;
+    public:
+        friend class Heap;
+};
+
+class Heap{
+    private:
+         Student a[30];
+         int n; //no of elements in the heap
+    public:
+
+        void create_heap(){
+            cout<<"enter no of elements of array\n";
+            cin>>n;
+            for (int i = 1; i <= n; i++) {
+                cout<<"Enter name: ";   
+                cin>>a[i].name;
+                cout<<"enter marks: ";
+                cin>>a[i].marks;
+            }
+          build_minheap(a, n);  
+        } 
+        
+        void display_heap(){
+            cout<<"-----------------------------------------"<<endl;
+            for (int i = 1; i <= n; i++) {
+                cout<<a[i].name<<"-"<<a[i].marks<<endl;
+            } 
+             cout<<"-----------------------------------------"<<endl;
+        }
+
+    private:
+        void min_heap(Student *a, int m, int n){
+            int j, t;
+            t= a[m].marks;
+            j = 2 * m;
+            while (j <= n) {
+                if (j < n && a[j+1].marks < a[j].marks)
+                    j = j + 1;
+                if (t < a[j].marks)
+                    break;
+                else if (t >= a[j].marks) {
+                    a[j/2] = a[j];
+                    j = 2 * j;
+                }
+            }
+            a[j/2].marks = t;
+            return;
+        }
+
+        void build_minheap(Student *a, int n) {
+            int k;
+            for(k = n/2; k >= 1; k--) {
+                min_heap(a,k,n);
+            }
+        }
+
+
+};
+
+ 
+int main() {
+    Heap h1;
+    h1.create_heap();
+    h1.display_heap();
+}
