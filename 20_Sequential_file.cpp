a15cf797421232ffec998e51252364a483bbb333
Neeraj Magdum <122902068+nirz306@users.noreply.github.com>
2024-05-05 12:18:16 +0530

    Update 20_Sequential_file.cpp

--- 20_Sequential_file.cpp
+++ 20_Sequential_file.cpp
@@ -1,3 +1,8 @@
+/*Department maintains a student information. The file contains roll number, name, division 
+and address. Allow user to add, delete information of student. Display information of 
+particular employee. If record of student does not exist an appropriate message is 
+displayed. If it is, then the system displays the student details. Use sequential file to 
+maintain the data*/
 #include <iostream>
 #include <fstream>
 using namespace std;
