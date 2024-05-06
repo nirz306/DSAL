6cd2584592a6a851a9f2aca8a3c38d985edc9a6a
Neeraj Magdum <122902068+nirz306@users.noreply.github.com>
2024-05-06 20:39:53 +0530

    Update 25_Prims.cpp

--- 25_Prims.cpp
+++ 25_Prims.cpp
@@ -1,133 +1,148 @@
-#include <iostream>
-#define I 9999
+#include<iostream>
+#include<vector>
 using namespace std;
+#define MAX 20
+#define I 9999     //infinity
 
-class graph {
-    int arr[7][7];
-    int near[7];
-    int visited[2][6]; 
-public:
-    graph() {
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 7; j++) {
-                arr[i][j] = I;
-            }
-            near[i] = I;
+class Graph{
+	int vertices;
+	int G[MAX][MAX];
+
+	public:
+        Graph()
+        {
+            vertices = 0;
         }
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 6; j++) {
-                visited[i][j] = -1; 
+        
+        void readGraph(){
+            cout<<"\nEnter no. of vertices : "; cin>>vertices;
+            int ans,weight;
+
+            for(int i = 0; i < vertices ; i++)
+            {
+                for(int j = i; j < vertices ; j++)
+                {
+                    if(i==j)
+                    {
+                        G[i][j]=I;
+                        
+                    }
+                    else
+                {
+                    
+                    cout<<"\nIs there an edge between "<<i<<" and "<<j<<" ? (1/0) : ";
+                    cin>>ans;
+                    
+                    if(ans == 1)
+                    {
+                        cout<<"\nEnter weight : ";
+                        cin>>weight;
+                        
+                        G[i][j] = G[j][i] = weight;
+                        
+                    }
+                    else
+                    {
+                        G[i][j] = G[j][i] = I;   // I = infinity
+                    }
+                }
+                }
             }
+        }
+        
+    private:
+        int find_min(int key[], bool mst[]){
+            int minn = I; // Initialize minn to infinity
+            int i; int ans;
+            for(i = 0; i < vertices; i++){
+                if(!mst[i] && key[i] < minn){ // Check if the vertex is not in MST and key is less than current minn
+                    minn = key[i];
+                    ans = i;
+                }
+            }
+            return ans;
+        }
+
+        vector <int> get_neighbour(int index){
+            vector<int> adj;
+            for(int i=0;i<vertices;i++){
+                if(G[i][index]!=I)
+                    adj.push_back(i);
+            }
+            return adj;
+        }
+    
+    public:
+
+        void printGraph(){
+            for(int i = 0; i < vertices; i++)
+            {
+                for(int j = 0; j < vertices ; j++)
+                {
+                    if(G[i][j] == I)
+                    {
+                        cout<<" I";
+                    }
+                    else
+                    {
+                        cout<<" "<<G[i][j];
+                    }
+                }
+            cout<<endl;
+            }   
+        }
+
+        int prims(){
+            bool mst[vertices];
+            int key[vertices];      //initially infinity
+            int parent[vertices];  //parent
+            
+            for(int i = 1; i < vertices; i++){
+                    key[i] = I;     // Initialize all other keys to infinity
+                    mst[i] = false;
+                    parent[i] = -1;
+            }
+
+        //start with the algo
+        key[0] = 0;
+       
+
+        for(int i = 0 ; i<vertices ; i++){
+            int index = find_min(key,mst);
+            
+            mst[index] = true;
+            vector <int> neighbours= get_neighbour(index);
+
+            for(int j=0;j<neighbours.size();j++){
+                int neigh = neighbours[j];
+                if(!mst[neigh]){
+                    int weight = G[index][neigh];
+                    if(weight<key[neigh])
+                        key[neigh]=weight;
+                }
+                parent[neigh] = index;
+            }
+        }
+
+        //display
+        for(int k=1;k<vertices;k++){
+            cout<<parent[k]<<"->"<<k<<" :"<<key[k]<<endl;
         }
     }
-    void insert();
-    void display();
-    void prims();
-    void cost();
 };
 
-void graph::insert() {
-    for (int i = 0; i < 7; i++) {
-        for (int j = i; j < 7; j++) {
-            if (i == j) {
-                arr[i][j] = I;
-            } else {
-                int ans;
-                cout << "Is there an edge between " << i << " and " << j << "(0/1): ";
-                cin >> ans;
-                if (ans == 1) {
-                    cout << "Enter weight: ";
-                    int weight;
-                    cin >> weight;
-                    arr[i][j] = arr[j][i] = weight;
-                } else {
-                    arr[i][j] = I;
-                }
-            }
-        }
-    }
+ int main()
+{
+	Graph g;
+	g.readGraph();
+	 
+	cout<<"\nOriginal graph : \n";
+	g.printGraph();
+	
+	cout<<"\nMST : \n";
+	 g.prims();
+	
+	 
 }
+ 
 
-void graph::display() {
-    for (int i = 0; i < 7; i++) {
-        for (int j = 0; j < 7; j++) {
-            if (arr[i][j] == I) {
-                cout << "  " << "I";
-            } else {
-                if (arr[i][j] >= 10) {
-                    cout << " " << arr[i][j];
-                } else {
-                    cout << "  " << arr[i][j];
-                }
-            }
-        }
-        cout << endl;
-    }
-    cout << endl;
-}
-
-void graph::prims() {
-    int u, v;
-    int min = I;
-    for (int i = 0; i < 7; i++) {
-        for (int j = i; j < 7; j++) {
-            if (arr[i][j] < min) {
-                min = arr[i][j];
-                u = i;
-                v = j;
-            }
-        }
-    }
-    visited[0][0] = u;
-    visited[1][0] = v;
-    near[u] = near[v] = -1;
-
-    for (int i = 0; i < 7; i++) {
-        if (near[i] != -1) {
-            if (arr[i][u] < arr[i][v]) {
-                near[i] = u;
-            } else {
-                near[i] = v;
-            }
-        }
-    }
-
-    for (int i = 1; i < 6; i++) { 
-        min = I;
-        int k;
-        for (int j = 0; j < 7; j++) {
-            if (near[j] != -1 && (arr[j][near[j]] < min)) {
-                min = arr[j][near[j]];
-                k = j;
-            }
-        }
-        visited[0][i] = k;
-        visited[1][i] = near[k];
-        near[k] = -1;
-        for (int i = 0; i < 7; i++) {
-            if (near[i] != -1 && (arr[i][k] < arr[i][near[i]])) {
-                near[i] = k;
-            }
-        }
-    }
-    for (int i = 0; i < 6; i++) {
-        cout << "(" << visited[0][i] << "," << visited[1][i] << ")" << endl;
-    }
-}
-
-void graph::cost() {
-    int cost1 = 0;
-    for (int i = 0; i < 6; i++) {
-        cost1 += arr[visited[0][i]][visited[1][i]];
-    }
-    cout << "\n Cost of spanning tree : "<<cost1;
-}
-
-int main() {
-    graph g;
-    g.insert();
-    g.display();
-    g.prims();
-    g.cost();
-    return 0;
-}
